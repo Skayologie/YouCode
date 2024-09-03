5008e548b18e800900352a7c080b2bb166a43e83
Jawad Boulmal <jawadboulmal@gmail.com>
2024-09-03 18:02:58 +0200

    updates

---
+++ Challenges/Day 01/Tables/Exercices sur les Tableaux en C/#04 Trouver le Maximum.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(){
+    //variables
+
+
+    int size;
+    printf("Enter the size : ");
+    scanf("%d",&size);
+    int table[size]  ;
+    for(int i= 0; i< size ; i++){
+        printf("Enter All Values : ");
+        scanf("%d",&table[i]);
+    }
+    int Grather=table[0];
+    for(int i= 0;i<size;i++){
+        if(table[i] >= Grather){
+            Grather = table[i];
+        }
+    }
+    printf("This is the Big Number : %d",Grather);
+
+
+
+    return 0;
+}
+
+
+
