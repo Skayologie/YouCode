dec5ea18d584968c362a04fbdd5771ee1bed8018
Jawad Boulmal <60289030+Skayologie@users.noreply.github.com>
2024-09-04 12:31:58 +0200

    Update #04 Inversion d'un Entier.c

--- Challenges/Day 02/Les Boucles/Exercices sur les Boucles en C/#04 Inversion d'un Entier.c
+++ Challenges/Day 02/Les Boucles/Exercices sur les Boucles en C/#04 Inversion d'un Entier.c
@@ -1,19 +1,6 @@
 #include <stdio.h>
 int main(){
-    int reverseNum  = 0 ;
-    char numbers[4];
-    int temp;
-    char start,end;
-
-    printf("Type 4 Numbers : ");
-
-    scanf("%s",&numbers);
-
-    start = numbers[0];
-    end = strlen(numbers);
-
-
-    printf("%s\n",start);
+    
 
 
 
